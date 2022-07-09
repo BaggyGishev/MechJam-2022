dfd72e3def1839aa7232475ffdd5ac1b94377b64
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-07-09 12:59:18 +0300

    improve grid system

--- MechJam-2022/Assets/_Project/Scripts/World/Grid.cs
+++ MechJam-2022/Assets/_Project/Scripts/World/Grid.cs
@@ -1,16 +1,45 @@
+using UnityEngine;
+
 namespace Gisha.MechJam.World
 {
     public class Grid
     {
-        public static Grid[,] Nodes { private set; get; }
+        public Grid[,] Cells { private set; get; }
+        public int Width { private set; get; }
+        public int Height { private set; get; }
+        public float CellSize { private set; get; }
 
-        public static void CreateGrid(int xSize, int ySize)
+        public Grid(int xSize, int ySize, float cellSize)
         {
-            Nodes = new Grid[xSize, ySize];
+            Width = xSize;
+            Height = ySize;
+            CellSize = cellSize;
+            Cells = new Grid[Width, Height];
+        }
+
+        public Vector2Int ConvertWorldPosToCoords(Vector3 worldPosition)
+        {
+            int xCoords = Mathf.FloorToInt(worldPosition.x / CellSize + Width / 2f);
+            int yCoords = Mathf.FloorToInt(worldPosition.z / CellSize + Height / 2f);
+
+            return new Vector2Int(xCoords, yCoords);
+        }
+
+        public Vector3 GetWorldPosFromCoords(int xCoords, int yCoords)
+        {
+            if (xCoords > Width || yCoords > Height || xCoords < 0 || yCoords < 0)
+            {
+                Debug.LogError("Out of coords.");
+                return Vector3.zero;
+            }
+
+            float worldX = (xCoords - Width / 2f + .5f) * CellSize;
+            float worldY = (yCoords - Height / 2f + .5f) * CellSize;
+            return new Vector3(worldX, 0f, worldY);
         }
     }
 
-    public class Node
+    public class Cell
     {
     }
 }
--- MechJam-2022/Assets/_Project/Scripts/World/WorldManager.cs
+++ MechJam-2022/Assets/_Project/Scripts/World/WorldManager.cs
@@ -4,21 +4,39 @@ namespace Gisha.MechJam.World
 {
     public class WorldManager : MonoBehaviour
     {
-        [SerializeField] private int xSize, ySize;
+        [SerializeField] private int width, height;
+        [SerializeField] private float cellSize;
+
+        private Grid _grid;
 
         private void Awake()
         {
-            Grid.CreateGrid(xSize, ySize);
+            _grid = new Grid(width, height, cellSize);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out var hitInfo))
+                {
+                    var coords = _grid.ConvertWorldPosToCoords(hitInfo.point);
+                    Debug.Log("Coords are: " + coords.x + " " + coords.y);
+                }
+            }
         }
 
         private void OnDrawGizmos()
         {
             if (!Application.isPlaying)
-                Grid.CreateGrid(xSize, ySize);
+                _grid = new Grid(width, height, cellSize);
 
-            for (int x = 0; x < Grid.Nodes.GetLength(0); x++)
-            for (int y = 0; y < Grid.Nodes.GetLength(1); y++)
-                Gizmos.DrawWireCube(new Vector3(x, 0, y), Vector3.one);
+            for (int x = 0; x < _grid.Cells.GetLength(0); x++)
+            for (int y = 0; y < _grid.Cells.GetLength(1); y++)
+                Gizmos.DrawWireCube(_grid.GetWorldPosFromCoords(x, y),
+                    new Vector3(_grid.CellSize, 0.25f, _grid.CellSize));
         }
     }
 }
