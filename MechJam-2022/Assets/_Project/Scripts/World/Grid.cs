b5c3c9a633b7aae19729581f170b960f6d92f1aa
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-07-10 12:34:48 +0300

    grid transform encapsulation

--- MechJam-2022/Assets/_Project/Scripts/World/Grid.cs
+++ MechJam-2022/Assets/_Project/Scripts/World/Grid.cs
@@ -10,7 +10,7 @@ namespace Gisha.MechJam.World
         public float CellSize { get; }
         public Cell FirstCell => Cells[0, 0];
         public Cell LastCell => Cells[Cells.GetLength(0) - 1, Cells.GetLength(1) - 1];
-        
+
         public Grid(int xSize, int ySize, float cellSize)
         {
             Width = xSize;
@@ -40,16 +40,16 @@ namespace Gisha.MechJam.World
         }
     }
 
-    public static class GridTransform
+    internal static class GridTransform
     {
-        public static Vector3 GetWorldPosFromCoords(Vector2Int coords, Grid grid)
+        internal static Vector3 GetWorldPosFromCoords(Vector2Int coords, Grid grid)
         {
             float worldX = (coords.x - grid.Width / 2f + .5f) * grid.CellSize;
             float worldY = (coords.y - grid.Height / 2f + .5f) * grid.CellSize;
             return new Vector3(worldX, 0f, worldY);
         }
 
-        public static Vector2Int GetCoordsFromWorldPos(Vector3 worldPosition, Grid grid)
+        internal static Vector2Int GetCoordsFromWorldPos(Vector3 worldPosition, Grid grid)
         {
             int xCoords = Mathf.FloorToInt(worldPosition.x / grid.CellSize + grid.Width / 2f);
             int yCoords = Mathf.FloorToInt(worldPosition.z / grid.CellSize + grid.Height / 2f);
@@ -57,7 +57,7 @@ namespace Gisha.MechJam.World
             return new Vector2Int(xCoords, yCoords);
         }
 
-        public static Cell[] GetCells(Grid grid, Vector3 position, Vector2Int areaDimensions, float yEulerAngles)
+        internal static Cell[] GetCells(Grid grid, Vector3 position, Vector2Int areaDimensions, float yEulerAngles)
         {
             int yRotation = Mathf.RoundToInt(yEulerAngles);
 
@@ -91,7 +91,7 @@ namespace Gisha.MechJam.World
             return result;
         }
 
-        public static Vector2 GetOffsetCoords(int xSize, int zSize)
+        private static Vector2 GetOffsetCoords(int xSize, int zSize)
         {
             float xOffset = xSize % 2 == 0 ? xSize / 2f - 0.5f : Mathf.Ceil(xSize / 2f);
             float zOffset = zSize % 2 == 0 ? zSize / 2f - 0.5f : Mathf.Ceil(zSize / 2f);
@@ -99,7 +99,7 @@ namespace Gisha.MechJam.World
             return new Vector2(xOffset, zOffset);
         }
 
-        public static Vector3 CenterVector3FromCoords(Grid grid, Vector2Int a, Vector2Int b)
+        internal static Vector3 CenterVector3FromCoords(Grid grid, Vector2Int a, Vector2Int b)
         {
             Vector3 firstPos = GetWorldPosFromCoords(a, grid);
             Vector3 lastPos = GetWorldPosFromCoords(b, grid);
