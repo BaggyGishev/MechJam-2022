3e0bf7984696dfd8a344f68509542b2f559b040d
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-07-11 23:27:18 +0300

    add simple units navigation

---
+++ MechJam-2022/Assets/_Project/Scripts/Core/UnitAI.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Gisha.MechJam.Core
+{
+    public class UnitAI : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [Space] [SerializeField] private float rotationSmoothness;
+
+
+        private NavMeshAgent _navMeshAgent;
+
+        private void Awake()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _navMeshAgent.updateRotation = false;
+        }
+
+        private void Start()
+        {
+            _navMeshAgent.SetDestination(target.position);
+        }
+
+        private void LateUpdate()
+        {
+            var rotation = Quaternion.LookRotation(_navMeshAgent.velocity.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSmoothness * Time.deltaTime);
+        }
+    }
+}
