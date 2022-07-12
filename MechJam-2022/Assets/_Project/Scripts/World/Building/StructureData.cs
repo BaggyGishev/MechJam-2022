bf1068677ed813a89ea97257087adc227117f490
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-07-12 16:12:04 +0300

    add navigation obstacle to structures

--- MechJam-2022/Assets/_Project/Scripts/World/Building/StructureData.cs
+++ MechJam-2022/Assets/_Project/Scripts/World/Building/StructureData.cs
@@ -7,6 +7,8 @@ namespace Gisha.MechJam.World.Building
     {
         [SerializeField] private string structureName;
         [SerializeField] private GameObject prefab;
+        [SerializeField] private Vector2Int dimensionsAddition = Vector2Int.one;
+
         public GameObject Prefab => prefab;
 
         public MeshFilter MeshFilter
@@ -17,9 +19,9 @@ namespace Gisha.MechJam.World.Building
         public Vector2Int GetDimensions(float cellSize)
         {
             int x = Mathf.CeilToInt((MeshFilter.sharedMesh.bounds.max.x - MeshFilter.sharedMesh.bounds.min.x) *
-                                     Prefab.transform.localScale.x / cellSize);
+                Prefab.transform.localScale.x / cellSize) + dimensionsAddition.x;
             int y = Mathf.CeilToInt((MeshFilter.sharedMesh.bounds.max.z - MeshFilter.sharedMesh.bounds.min.z) *
-                                     Prefab.transform.localScale.z / cellSize);
+                Prefab.transform.localScale.z / cellSize) + dimensionsAddition.y;
 
             return new Vector2Int(x, y);
         }
