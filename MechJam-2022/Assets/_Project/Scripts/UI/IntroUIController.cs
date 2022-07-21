86d1e67bab547823dcb84ecbca44876364c94f8a
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-07-22 00:19:26 +0300

    add intro tutorial

---
+++ MechJam-2022/Assets/_Project/Scripts/UI/IntroUIController.cs
@@ -0,0 +1,32 @@
+﻿using Gisha.Effects.Audio;
+using UnityEngine;
+
+namespace Gisha.MechJam.UI
+{
+    public class IntroUIController : MonoBehaviour
+    {
+        [SerializeField] private GameObject popup;
+        [SerializeField] private GameObject[] pages;
+
+        private int _pageIndex;
+
+        private void Awake()
+        {
+            popup.SetActive(true);
+            _pageIndex = 0;
+            pages[_pageIndex].SetActive(true);
+        }
+
+        public void OnClick_Ok()
+        {
+            pages[_pageIndex].SetActive(false);
+            _pageIndex++;
+            if (_pageIndex < pages.Length)
+                pages[_pageIndex].SetActive(true);
+            else
+                popup.SetActive(false);
+            
+            AudioManager.Instance.PlaySFX("click");
+        }
+    }
+}
